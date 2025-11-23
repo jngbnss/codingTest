994a01af4e07d3741443d4fd693ccbfc8c5943d8
jngbnss <jngbnmn@naver.com>
2025-11-23 21:28:57 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,b,ret,x,temp;
+int A,B,C;
+string s;
+vector<int>v;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>a>>b;
+		cout<<"Case #"<<i<<": "<<a+b<<'\n';
+	}
+}
