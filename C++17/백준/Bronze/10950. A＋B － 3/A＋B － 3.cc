27ed27af643a4eeccaa4c1691fb9a7d77cbf1e2f
jngbnss <jngbnmn@naver.com>
2025-11-23 21:17:00 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,b,ret;
+int A,B,C;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>a>>b;
+		cout<<a+b<<'\n';
+	}
+}
