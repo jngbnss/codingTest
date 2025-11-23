7515aa5c621a9db89a7261e7fd2865391901a2ec
jngbnss <jngbnmn@naver.com>
2025-11-23 21:09:56 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,b,ret;
+int A,B,C;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	cin>>a;
+	for(int i=1;i<=a;i++){
+		for(int j =0;j<i;j++){
+			cout<<"*";
+		}cout<<'\n';
+	}
+}
