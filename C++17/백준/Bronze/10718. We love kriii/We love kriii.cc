ae9da6df5f010fa4b98d5809eccfaf8ceba763ce
jngbnss <jngbnmn@naver.com>
2025-11-23 21:18:52 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,b,ret;
+int A,B,C;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	cout<<"강한친구 대한육군\n강한친구 대한육군";
+}
