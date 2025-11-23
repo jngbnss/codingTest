06bcb06fb5d5680c0534689779cc29bf2ec2abc2
jngbnss <jngbnmn@naver.com>
2025-11-23 21:32:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,b,ret,x,temp;
+int A,B,C;
+char c;
+string s;
+vector<int>v;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	cin>>c;
+	cout<<(int)c;
+}
