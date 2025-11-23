b0085e4c88fb43b7fcb60cf3d44d1820d392f392
jngbnss <jngbnmn@naver.com>
2025-11-23 21:38:12 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,b,ret,x,temp;
+long long  A,B,C;
+char c;
+
+string s;
+vector<int>v;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	cin>>a>>b;
+	cout<<a+b;
+}
