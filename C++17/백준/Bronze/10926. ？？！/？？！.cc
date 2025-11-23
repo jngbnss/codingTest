546662f7b5d1d6b39e59bb861e9db4794b2b7800
jngbnss <jngbnmn@naver.com>
2025-11-23 21:26:59 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,b,ret,x,temp;
+int A,B,C;
+string s;
+vector<int>v;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	cin>>s;
+	cout<<s<<"??!";
+}
