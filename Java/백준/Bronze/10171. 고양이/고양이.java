db3ada4a31660fc383d79c326a26d98db48d7df1
jngbnss <jngbnmn@naver.com>
2025-11-23 21:13:38 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 14064 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,10 @@
+//package ps.baekjoon.level1.q10171;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n"
+                + " )  ( ')\n"
+                + "(  /  )\n"
+                + " \\(__)|");
+    }
+}
