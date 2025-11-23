bc4605a3b3bda9033e45c6577067239e448756d9
jngbnss <jngbnmn@naver.com>
2025-11-23 21:45:38 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,b,ret,x,temp;
+long long  A,B,C;
+char c;
+int arr[]={1,1,2,2,2,8};
+
+string s;
+vector<int>v;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	cout<<33<<'\n'<<"jngbnss";
+		
+}
