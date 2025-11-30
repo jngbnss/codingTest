66ac3dd12d5865f4a7da1827d25f3e534801a2e8
jngbnss <jngbnmn@naver.com>
2025-11-30 15:16:51 +0900

    [Bronze IV] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a,b,c;
+vector<int>v;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	
+	for(int i=0;i<3;i++){
+		int temp;
+		cin>>temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(),v.end());
+	cout<<v[1];
+}
