469c998b5408a3cb26c4af24e1eecdf7c487f3d9
jngbnss <jngbnmn@naver.com>
2025-11-30 18:55:24 +0900

    [Bronze IV] Title: 주사위, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,d,a,b;
+string s;
+vector<int>v;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>a>>b;
+		cout<<"Case "<<i<<": "<<a+b<<'\n';
+	}
+		
+}
