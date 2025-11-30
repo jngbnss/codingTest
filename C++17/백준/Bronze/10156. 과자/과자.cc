65852e34ec6eabd421ee78d425502ab861542a37
jngbnss <jngbnmn@naver.com>
2025-11-30 15:36:52 +0900

    [Bronze IV] Title: 과자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10156. 과자/과자.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long n,a,b,c,sum,mn=1e9,ret=-50;
+vector<int>v;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
+	cin>>a>>b>>c;
+	if(a*b-c>0) cout<<a*b-c;
+	else cout<<0;
+		
+	
+}
