5afa0bc6439e5aafbdfbc4371b5937949661e7b8
jngbnss <jngbnmn@naver.com>
2025-11-30 18:22:46 +0900

    [Bronze IV] Title: !밀비 급일, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11365. ！밀비 급일/！밀비 급일.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,x,y,ret,sum,l,a,b,c,d;
+string s;
+vector<int>v,v1;
+
+int main(){
+	while(true){
+		getline(cin,s);
+		if(s=="END") return 0;
+		reverse(s.begin(),s.end());
+		cout<<s<<'\n';
+	}
+		
+}
