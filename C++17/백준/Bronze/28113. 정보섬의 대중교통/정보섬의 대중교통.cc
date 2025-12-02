350f769bfc7e15ffa71ee861ddb47d0a876f9b96
jngbnss <jngbnmn@naver.com>
2025-12-03 00:02:10 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long n,ret=1,b,c,a;
+
+int main(){
+	cin>>n>>a>>b;
+	
+	if(b==a) cout<<"Anything";
+	else if(a<b) cout<<"Bus";
+	else cout<<"Subway";
+	
+}
