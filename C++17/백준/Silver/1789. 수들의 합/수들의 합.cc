555890baa623ff0c31c8a80ca667c8ffcaf249d9
jngbnss <jngbnmn@naver.com>
2025-12-07 13:21:34 +0900

    [Silver V] Title: 수들의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/1789. 수들의 합/수들의 합.cc
+++ C++17/백준/Silver/1789. 수들의 합/수들의 합.cc
@@ -6,10 +6,6 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);cout.tie(0);
 	cin>>n;
-	if(n==1){
-		cout<<1;
-		return 0;
-	}
 	for(long long i=0;i<=n;i++){
 		ret+=i;
 		if(ret==n){ cout<<i; return 0;}
