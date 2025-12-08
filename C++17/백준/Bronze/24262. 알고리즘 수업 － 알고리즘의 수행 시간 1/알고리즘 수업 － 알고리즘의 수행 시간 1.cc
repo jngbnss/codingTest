09072b3985816a228f58fd0a0438a401afee56b8
jngbnss <jngbnmn@naver.com>
2025-12-09 08:48:29 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,ret,cnt;
+string s;
+char arr[30][30];
+int visited[30][30];
+vector<int>v;
+int dy[]={-1,0,1,0},dx[]={0,-1,0,1};
+int main(){
+	cin>>n;
+	cout<<1<<'\n'<<0;
+	
+}
+	
