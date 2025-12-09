b07c848ce51742bb20fd83140b27ab69a674c2db
jngbnss <jngbnmn@naver.com>
2025-12-09 11:10:55 +0900

    [Bronze III] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long n,m,a,c,d,e,cnt,ret,temp;
+char b;
+vector<long long>v;
+int main(){
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>a>>b>>c;
+		cout<<a+c<<'\n';
+	}
+}
