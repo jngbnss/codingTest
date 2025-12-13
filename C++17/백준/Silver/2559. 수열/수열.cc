64c58d3ab64628494fdde02f69ab2cfe707b2ace
jngbnss <jngbnmn@naver.com>
2025-12-13 19:47:28 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 3584 KB -BaekjoonHub

--- C++17/백준/Silver/2559. 수열/수열.cc
+++ C++17/백준/Silver/2559. 수열/수열.cc
@@ -3,30 +3,21 @@ using namespace std;
 typedef long long ll;
 ll n,m,a,b,temp,ret;
 ll arr[100004],sum[100004];
-
 int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);cout.tie(0);
 	cin>>n>>m;
-	
 	for(ll i=0;i<n;i++){
 		cin>>temp;
 		arr[i] = temp;
 	}
-	
 	for(ll i=0;i<m;i++){
 		sum[0]+=arr[i];
 	}
 	ret = sum[0];
 	for(ll i=1;i<=n-m;i++){
-//		cout<<sum[i-1];
 		sum[i] = sum[i-1]+arr[i+m-1]-arr[i-1];
 		ret = max(ret,sum[i]);
-//		cout<<'\n';
 	}
-//	for(ll i=0;i<n-m;i++){
-//		cout<<sum[i]<<'\n';
-//	}
 	cout<<ret;
-	
-	
-	 
 }
