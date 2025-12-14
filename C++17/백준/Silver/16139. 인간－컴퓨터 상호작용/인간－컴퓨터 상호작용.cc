d8f998303f7dc9f26afe9d410d7fc878db56b66a
jngbnss <jngbnmn@naver.com>
2025-12-14 11:59:25 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 88 ms, Memory: 22844 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ C++17/백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -1,33 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 string s;
 long long n,a,b,cnt;
 char c;
-string psum[200004];
-
+//string psum[3000000];
+int psum[26][200001];
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);cout.tie(0);
 	cin>>s;
-	for(int i=1;i<=s.size();i++){
-		psum[i] = psum[i-1]+s[i-1];
+	for(ll i=1;i<=s.size();i++){
+		for(int c = 0;c<26;c++){
+			psum[c][i] = psum[c][i-1];
+		}
+		psum[s[i-1]-'a'][i]++;
 	}
-	for(int i=1;i<=s.size();i++){
-//		cout<<i<<" : "<<psum[i]<<'\n';
-	}
+//	for(ll i=1;i<=s.size();i++){
+////		cout<<i<<" : "<<psum[i]<<'\n';
+//	}
 	cin>>n;
-	for(int i=0;i<n;i++){
+	for(ll i=0;i<n;i++){
 		cnt = 0;
 		cin>>c>>a>>b;
-		for(auto i:psum[b+1]){
-			if(i==c) cnt++;
-		}
-		
-//		cout<<cnt<<' ';
-		
-		for(auto i:psum[a]){
-			if(i==c) cnt--;
-		}
-		cout<<cnt<<'\n';
+		int idx =c -'a';
+		cout<<psum[idx][b+1]-psum[idx][a]<<'\n';
+//		
+//		for(auto i:psum[b+1]){
+//			if(i==c) cnt++;
+//		}
+//		
+////		cout<<cnt<<' ';
+//		
+//		for(auto i:psum[a]){
+//			if(i==c) cnt--;
+//		}
+//		cout<<cnt<<'\n';
 	}
 }
