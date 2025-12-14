263031412620ebd1132f7b84967e4fdb7f37c996
jngbnss <jngbnmn@naver.com>
2025-12-14 20:49:18 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    // cout<<commands.size();
+    for(int i=0;i<commands.size();i++){
+        vector<int>temp(array.begin()+commands[i][0]-1,array.begin()+commands[i][1]);
+    sort(temp.begin(),temp.end());
+    answer.push_back(temp[commands[i][2]-1]);
+    }
+    
+    return answer;
+}
