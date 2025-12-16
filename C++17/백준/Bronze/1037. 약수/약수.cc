1271955c0e453b2f7b7d6f1f1d414a08a57f1a7a
jngbnss <jngbnmn@naver.com>
2025-12-16 21:17:11 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,a;
+vector<int>v;
+int main(){
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cin>>a;
+		v.push_back(a);
+	}
+	sort(v.begin(),v.end());
+//	for(auto i:v){
+//		cout<<i<<'\n';
+//	}
+	cout<<v[0]*v[v.size()-1];
+}
