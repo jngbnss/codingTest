47e7f9ed907ff61c9e43b4b41fcbddd229e34cd5
jngbnss <jngbnmn@naver.com>
2025-12-16 20:54:34 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- C++17/백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ C++17/백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -2,40 +2,43 @@
 using namespace std;
 int n;
 string s;
-char arr[70][70]; // 1~64
+char arr[100][100];
 
 string go(int y,int x,int s){
 	if(s==1) return string(1,arr[y][x]);
-	string ret="";
-	char c = arr[y][x];
+	
+	string ret ="";
+	char c=arr[y][x];
 	
 	for(int i=y;i<y+s;i++){
 		for(int j=x;j<x+s;j++){
 			if(c!=arr[i][j]){
-				ret+='(';
+				ret+="(";
 				ret+=go(y,x,s/2);
 				ret+=go(y,x+s/2,s/2);
 				ret+=go(y+s/2,x,s/2);
 				ret+=go(y+s/2,x+s/2,s/2);
-				ret+=')';
+				ret+=")";
 				return ret;
 			}
 		}
 	}
+			
+	
 	return string(1,arr[y][x]);
 }
-
-int main(){
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);cout.tie(0);
 	
+	
+	
+	
+int main(){
 	cin>>n;
+	
 	for(int i=0;i<n;i++){
 		cin>>s;
 		for(int j=0;j<n;j++){
 			arr[i][j] = s[j];
 		}
 	}
-	
 	cout<<go(0,0,n);
 }
