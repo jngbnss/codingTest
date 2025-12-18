3500b37c7f970a0ea2192b8e579b98f01adbdb2a
jngbnss <jngbnmn@naver.com>
2025-12-18 18:28:46 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -0,0 +1,37 @@
+// lots, first
+
+#include<bits/stdc++.h>
+using namespace std;
+int n,c,temp;
+vector<int>v;
+map<int,int>cnt,order;
+
+bool cmp(int a, int b){
+	if(cnt[a]==cnt[b]){
+		return order[a]<order[b];
+	}
+	return cnt[a]>cnt[b];
+}
+
+int main(){
+	cin>>n>>c;
+	
+	for(int i=0;i<n;i++){
+		cin>>temp;
+		v.push_back(temp);
+		
+		cnt[temp]++;
+		if(order[temp]==0){
+			order[temp] = i+1;
+		}
+	}
+	
+//	for(auto i:mp){
+//		cnt.push_back({i.second,i.first});
+//	}
+	
+	sort(v.begin(),v.end(),cmp);
+	for(auto i:v){
+		cout<<i<<' ';
+	}
+}
