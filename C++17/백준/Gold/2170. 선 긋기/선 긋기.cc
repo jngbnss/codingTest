2b15313f2dfc07feeaf2924ad17c1e229c788004
jngbnss <jngbnmn@naver.com>
2025-12-20 21:15:45 +0900

    [Gold V] Title: 선 긋기, Time: 352 ms, Memory: 14440 KB -BaekjoonHub

--- C++17/백준/Gold/2170. 선 긋기/선 긋기.cc
+++ C++17/백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -4,12 +4,7 @@ typedef long long ll;
 ll n,a,b,ret;
 vector<pair<int,int>>v;
 
-bool cmp(pair<int,int>a,pair<int,int>b){
-	if(a.first == b.first){
-		return a.second>b.second;
-	}
-	return a.first<b.first;
-}
+
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);cout.tie(0);
@@ -20,7 +15,7 @@ int main(){
 		v.push_back({a,b});
 	}
 	
-	sort(v.begin(),v.end(),cmp);
+	sort(v.begin(),v.end());
 	
 //	for(auto i:v){
 //		cout<<i.first<<' '<<i.second<<'\n';
