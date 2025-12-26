b1560117fa27d9cf8f0d163c45c86bac08717c40
jngbnss <jngbnmn@naver.com>
2025-12-26 13:15:02 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,ret,a;
+string s;
+vector<int>v;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		cin>>n;
+		for(int i=0;i<n;i++){
+			cout<<'=';
+		}cout<<'\n';
+	}
+	
+}
