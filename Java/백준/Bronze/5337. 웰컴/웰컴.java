0b145e1b3378ab1314cedc9da1c7ea60f31013d5
jngbnss <jngbnmn@naver.com>
2025-12-26 10:38:38 +0900

    [Bronze V] Title: 웰컴, Time: 104 ms, Memory: 13888 KB -BaekjoonHub

---
+++ Java/백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,9 @@
+//package baekjoon.b5.q1809;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n"
+                + "|  | _ | _. _ ._ _  _\n"
+                + "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
