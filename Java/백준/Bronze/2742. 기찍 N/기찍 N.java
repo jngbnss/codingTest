fc3e5561ac334e22d69c65a32e00daaa1f7e761f
jngbnss <jngbnmn@naver.com>
2025-12-31 14:50:57 +0900

    [Bronze V] Title: 기찍 N, Time: 764 ms, Memory: 31496 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,15 @@
+//package d1231.b5.qb5;
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
+        }
+
+
+    }
+}
