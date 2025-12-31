657b3a3e0a01806774819c50eb6b26e2840c55b6
jngbnss <jngbnmn@naver.com>
2025-12-31 15:25:48 +0900

    [Bronze V] Title: We love kriii, Time: 96 ms, Memory: 14060 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,16 @@
+//package d1231.b5.qb5;
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int y = Integer.parseInt(br.readLine());
+//        int B = Integer.parseInt(br.readLine());
+//        int A = Integer.parseInt(st.nextToken());
+//        int B = Integer.parseInt(st.nextToken());
+            
+        System.out.println("강한친구 대한육군\n"
+                + "강한친구 대한육군");
+    }
+}
