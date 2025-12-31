30d002c15a675bc8132fffa4619a5702217f8856
jngbnss <jngbnmn@naver.com>
2025-12-31 15:32:12 +0900

    [Bronze IV] Title: 영수증, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

---
+++ Java/백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,24 @@
+//package d1231.b5.qb5;
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int sum =0;
+        for (int i = 0; i < n; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum+=a*b;
+        }
+        if (sum == x) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
