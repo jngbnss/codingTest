085d948c633ace71d0488bd2a55e7c1b7a30df08
jngbnss <jngbnmn@naver.com>
2025-12-31 15:07:07 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+//package d1231.b5.qb5;
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        System.out.println(s+"??!");
+
+
+    }
+}
