a4b6f792ef1c690b0cb888ad1fb5921eb9882f92
jngbnss <jngbnmn@naver.com>
2025-12-31 19:33:38 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ C++17/백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,40 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-int a[9];
-int n =9,r = 7;
-
-
-void solve(){
-	int sum =0;
-	for(int i=0;i<r;i++){
-		sum+=a[i];
-	}if(sum==100){
-		sort(a,a+7);
-		for(int i=0;i<r;i++) cout<<a[i]<<'\n';
-		exit(0);
-	}
-}
-
-void makePermutation(int n,int r,int depth){
-	if(r==depth){
-		solve();
-		return;
-	}
-	for(int i=depth;i<n;i++){
-		swap(a[i],a[depth]);
-		makePermutation(n,r,depth+1);
-		swap(a[i],a[depth]);
-	}
-	return;
-}
-
+int n,ret,sum;
+vector<int>v;
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);cout.tie(0);
 	
 	for(int i=0;i<9;i++){
-		cin>>a[i];
+		cin>>n;
+		v.push_back(n);
+		sum+=n;
 	}
+	sort(v.begin(),v.end());
 	
-	makePermutation(n,r,0);
+	int a,b;
+	bool check = false;
+	for(int i=0;i<9;i++){
+		for(int j=i+1;j<9;j++){
+			if(sum-v[i]-v[j]==100){
+				a =i,b=j;
+				check = true;
+			}
+			if(check) break;
+		}if(check) break;
+	}
+	for(int i=0;i<9;i++){
+		if(i==a||i==b) continue;
+		cout<<v[i]<<'\n';
+	}
 }
+		
+			
+			
