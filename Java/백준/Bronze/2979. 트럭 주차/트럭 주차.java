27cf165e7be3da110fec9f4cc8abc2b45c0f6be1
jngbnss <jngbnmn@naver.com>
2026-01-01 20:15:09 +0900

    [Bronze II] Title: 트럭 주차, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- Java/백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ Java/백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -1,32 +1,37 @@
-//package algorithm.week1.C2979;
+//package d260101.q2979;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
         int[] arr = new int[104];
-        for (int i = 0; i < 3; i++) {
-            int x = scanner.nextInt();
-            int y = scanner.nextInt();
-            for (int j = x; j < y; j++) {
+        for(int i=0;i<3;i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            for (int j = start; j <end ; j++) {
                 arr[j]++;
             }
         }
         int ret = 0;
-        for (int i = 0; i < 100; i++) {
-            if (arr[i] == 3) {
-                ret += c * 3;
-            } else if (arr[i] == 2) {
-                ret += b * 2;
-            } else if (arr[i] == 1) {
-                ret += a;
+        for (int i = 0; i < 101; i++) {
+            if(arr[i]==1){
+                ret+=a;
+            }else if(arr[i]==2){
+                ret+=2*b;
+            }else if(arr[i]==3){
+                ret+=3*c;
             }
-
         }
         System.out.println(ret);
+
     }
 }
