341ab1659144470ed60662cf9f6d1962ca47cf76
jngbnss <jngbnmn@naver.com>
2026-01-06 12:47:21 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder(my_string);
+        String answer = sb.reverse().toString();
+        return answer;
+    }
+}
