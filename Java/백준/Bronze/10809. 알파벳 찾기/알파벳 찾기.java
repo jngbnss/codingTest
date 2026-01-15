5f9c2c0460d58ff119f91f272ebbd77cf8559065
jngbnss <jngbnmn@naver.com>
2026-01-15 22:20:10 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 120 ms, Memory: 15904 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        int[] arr = new int[26];
+        for (int i = 0; i < 26; i++) {
+            arr[i] = -1;
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(arr[c-'a']==-1){
+                arr[c-'a']=i;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
