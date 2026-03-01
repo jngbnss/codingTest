98ff01434b259651b150a64f51bafc876268f4dd
jngbnss <jngbnmn@naver.com>
2026-03-01 21:38:49 +0900

    [Silver III] Title: N과 M (1), Time: 200 ms, Memory: 33832 KB -BaekjoonHub

---
+++ Java/백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static StringBuilder sb;
+    static ArrayList<Integer> list;
+    static boolean visited[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        sb = new StringBuilder();
+        list = new ArrayList<>();
+        visited = new boolean[n + 1];
+
+        go(0, list);
+        System.out.println(sb.toString());
+
+    }
+
+    static void go(int idx, ArrayList<Integer> list) {
+
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(list.get(i)).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                list.add(i);
+                go(idx + 1, list);
+                list.remove(list.size() - 1);
+                visited[i] = false;
+
+            }
+        }
+
+    }
+}
