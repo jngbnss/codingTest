d2b9a3364de8e92cb8f6ba3dcee0ae661d4bdb31
jngbnss <jngbnmn@naver.com>
2026-03-08 07:34:57 +0900

    [Bronze V] Title: Java Warriors, Time: 68 ms, Memory: 11484 KB -BaekjoonHub

---
+++ Java/백준/Bronze/30328. Java Warriors/Java Warriors.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println(n*4000);
+    }
+}
