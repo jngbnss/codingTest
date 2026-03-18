8134d60a10f51f723e7e74080a9cc80acd5d1ccc
jngbnss <jngbnmn@naver.com>
2026-03-18 10:27:05 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 64 ms, Memory: 11480 KB -BaekjoonHub

---
+++ Java/백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int n;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        System.out.println(":fan::fan::fan:\n"
+                + ":fan::"+line+"::fan:\n"
+                + ":fan::fan::fan:");
+    }
+}
