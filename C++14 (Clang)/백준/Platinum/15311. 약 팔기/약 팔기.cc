bd163cef9e418ad2a243f39cc5c62d18ff5269ea
jngbnss <jngbnmn@naver.com>
2026-03-23 14:08:14 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ C++14 (Clang)/백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+	cout<<2000<<'\n';
+	
+	for(ll i =0;i<1000;i++){
+		cout<<1<<' ';
+	}
+	for( ll i=0;i<1000;i++){
+		cout<<1000<<' ';
+	}
+}
