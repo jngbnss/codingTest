30d6019e9bac1b656524cea2eba2adfbcf761443
jngbnss <jngbnmn@naver.com>
2026-03-23 15:19:35 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 472 ms, Memory: 49068 KB -BaekjoonHub

--- Java/백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ Java/백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,97 +1,71 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-    static int n, m;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        root = new int[n + 1];
-
-
-        for (int i = 0; i <= n; i++) {
-            root[i] = i;
-        }
-
-//        list = new ArrayList[n + 1];
-//
-//        visited = new int[n + 1];
-//
-//        for (int i = 0; i <= n; i++) {
-//            list[i] = new ArrayList<>();
-//        }
-
-
-
-        //pq하면서 연결하고 다 방문하면 끝?
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int w = Integer.parseInt(st.nextToken());
-//            list[from].add(new Edge(from,to, w));
-            pq.offer(new Edge(from,to,w));
-            // 연결했다 치고
-        }
-
-//        pq.offer(new Edge(1, 0));
-        int ret = 0;
-
-        while (!pq.isEmpty()) {
-            Edge poll = pq.poll();
-            int from = poll.from;
-            int to = poll.to;
-            int w = poll.w;
-
-            if(find(from)!=find(to)){
-                union(from,to);
-//                System.out.println(w);
-                ret+=w;
-            }
-
-        }
-        System.out.println(ret);
-    }
-
-    static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            root[a] = b;
-        }
-    }
-
-    static int root[];
-
-    static int find(int x) {
-        if (root[x] == x) {
-            return x;
-        } else {
-            return  root[x] = find(root[x]);
-        }
-    }
-
-    // 크루스칼은 유니온파인드인데
-    static int visited[];
-    static ArrayList<Edge> list[];
-
-    static class Edge implements Comparable<Edge> {
+    static int n,m,ret;
+    static class Node implements Comparable<Node>{
         int from;
         int to;
         int w;
 
-        public Edge(int from, int to, int w) {
+        public Node(int from, int to, int w) {
             this.from = from;
             this.to = to;
             this.w = w;
         }
 
         @Override
-        public int compareTo(Edge o) {
-            return this.w - o.w;
+        public int compareTo(Node o) {
+            return this.w-o.w;
         }
+    }
+    static ArrayList<Node>list;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        ret =0 ;
+        list = new ArrayList<>();
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list.add(new Node(a,b,c));
+
+        }
+        Collections.sort(list);
+
+        root = new int[n+1];
+        for(int i=0;i<=n;i++){
+            root[i] = i;
+        }
+
+        for(int i=0;i<list.size();i++){
+            Node node = list.get(i);
+            int from = node.from;
+            int to = node.to;
+            int w = node.w;
+            if(find(from)!=find(to)){
+                uion(from,to);
+                ret+=w;
+            }
+        }
+        System.out.println(ret);
+    }
+    static int root[];
+    static void uion(int a,int b){
+        a = find(a);
+        b = find(b);
+        if(a!=b){
+            root[a] = b;
+        }
+    }
+    static int find(int x){
+        if(x==root[x]){
+            return x;
+        }
+        return root[x] = find(root[x]);
+
     }
 }
