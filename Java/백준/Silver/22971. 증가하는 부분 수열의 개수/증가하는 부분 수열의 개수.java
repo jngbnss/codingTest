d2a2f43607ad1665844f58fc8c28121f3d408b70
jngbnss <jngbnmn@naver.com>
2026-03-31 13:34:00 +0900

    [Silver II] Title: 증가하는 부분 수열의 개수, Time: 236 ms, Memory: 14296 KB -BaekjoonHub

--- Java/백준/Silver/22971. 증가하는 부분 수열의 개수/증가하는 부분 수열의 개수.java
+++ Java/백준/Silver/22971. 증가하는 부분 수열의 개수/증가하는 부분 수열의 개수.java
@@ -2,41 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static int arr[];
-    static long memo[];
-    static long mod = 998244353;
+    static int n, m;
+    static long arr[], dp[];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
-         arr = new int[n];
-         memo = new long[n];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++){
+        arr = new long[n];
+        dp = new long[n];
+
+        for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
 
         }
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
 
-        StringBuilder sb = new StringBuilder();
-
-        for(int i=0;i<n;i++){
-            memo[i] = 1; // 자기 자신만으로 구성된 수열 1개
-
-
-            // i 이전의 원소들을 탐색
-            for(int j=0;j<i;j++){
-                if(arr[j]<arr[i]){
-                    memo[i] = (memo[i]+memo[j]) % mod;
+                    dp[i] = (dp[i] + dp[j]) % 998244353;
 
                 }
             }
-            sb.append(memo[i]).append(" ");
         }
 
-        System.out.println(sb.toString());
-
+        for (int i = 0; i < n; i++) {
+            System.out.print(dp[i] + " ");
+        }
     }
 }
