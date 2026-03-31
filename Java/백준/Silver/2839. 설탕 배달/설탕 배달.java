e6cf3e1eff39e085a8b3429345cb6bba1d8ac234
jngbnss <jngbnmn@naver.com>
2026-03-31 11:31:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 11928 KB -BaekjoonHub

--- Java/백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ Java/백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,30 +1,27 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException{
+    static int n, m;
+    static long dp[];
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp = new long[50001];
+        Arrays.fill(dp, 9999);
+        dp[3] = 1;
+        dp[5] = 1;
 
-        int n = Integer.parseInt(br.readLine());
-        int ret = 0;
-        while(true){
-            if(n%5!=0){
-                ret++;
-                n-=3;
-                if(n<0){
-                    ret=-1;
-                    break;
-
-                }
-            }else if(n%5==0){
-                ret+=n/5;
-                break;
-            }else{
-                ret = -1;
-                break;
+        for (int i = 6; i <= n; i++) {
+            if (dp[i - 3] < 9999 || dp[i - 5] < 9999) {
+                dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
             }
-
         }
-
-        System.out.println(ret);
+        if (dp[n] >= 9999) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[n]);
+        }
     }
 }
