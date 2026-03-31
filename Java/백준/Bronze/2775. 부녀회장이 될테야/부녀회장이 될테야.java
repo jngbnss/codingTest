76bda4a6565f95188f755f86672e360e4076e279
jngbnss <jngbnmn@naver.com>
2026-03-31 17:06:51 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 68 ms, Memory: 11324 KB -BaekjoonHub

--- Java/백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ Java/백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -2,34 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n;
+    static long dp[][];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // 1. 아파트 만들기 (최대 14층, 14호이므로 15x15면 충분합니다)
-        int[][] apt = new int[15][15];
-
-        // 2. 0층 초기화 (0층 i호에는 i명이 산다)
-        for (int i = 1; i < 15; i++) {
-            apt[0][i] = i;
-        }
-
-        // 3. 나머지 층 채우기 (DP)
-        for (int i = 1; i < 15; i++) { // 1층부터 14층까지
-            for (int j = 1; j < 15; j++) { // 1호부터 14호까지
-                // 핵심: i층 j호 = i층 j-1호(옆집까지의 합) + i-1층 j호(내 아랫집)
-                apt[i][j] = apt[i][j - 1] + apt[i - 1][j];
-            }
-        }
-
-        // 4. 테스트케이스 처리
         int t = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < t; i++) {
-            int k = Integer.parseInt(br.readLine()); // 층
-            int n = Integer.parseInt(br.readLine()); // 호
-            sb.append(apt[k][n]).append("\n");
+
+        dp = new long[20][20];
+        for (int i = 0; i < 16; i++) {
+            dp[0][i] = i; //0층 1호 는 1명이 삽니다.
+//            dp[i][0] = 0;
         }
-        System.out.print(sb);
+        for (int i = 0; i < t; i++) {
+            //k층 n호
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            long ret = go(k, n);
+            System.out.println(ret);
+        }
+
     }
+
+    static long go(int k, int n) {
+        //기저사례
+        if (k == 0) {
+            return n;
+        }
+        if (n == 0) {
+            return 0;
+        }
+
+        //메모이제이션
+        if (dp[k][n] != 0) {
+            return dp[k][n];
+        }
+
+        //로직
+        return dp[k][n] = go(k - 1, n) + go(k, n - 1);
+
+    }
+
 }
