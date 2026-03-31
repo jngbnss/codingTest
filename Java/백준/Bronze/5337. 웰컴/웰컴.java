dc9ed40c8d0474e9006098a88e8ea7c9f7970052
jngbnss <jngbnmn@naver.com>
2026-04-01 08:34:22 +0900

    [Bronze V] Title: 웰컴, Time: 56 ms, Memory: 11200 KB -BaekjoonHub

--- Java/백준/Bronze/5337. 웰컴/웰컴.java
+++ Java/백준/Bronze/5337. 웰컴/웰컴.java
@@ -1,9 +1,16 @@
-//package baekjoon.b5.q1809;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    static int n;
+    static long dp[];
+    public static void main(String[] args) throws IOException{
         System.out.println(".  .   .\n"
                 + "|  | _ | _. _ ._ _  _\n"
                 + "|/\\|(/.|(_.(_)[ | )(/.");
+
     }
+
 }
