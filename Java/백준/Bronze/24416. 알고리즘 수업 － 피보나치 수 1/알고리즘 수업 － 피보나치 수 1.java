2dd51647cb52c941428a54da4cc28a2ee76fea75
jngbnss <jngbnmn@naver.com>
2026-03-31 10:37:08 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 72 ms, Memory: 11644 KB -BaekjoonHub

---
+++ Java/백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+    static int n, m;
+    static int arr[];
+    static long dp[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        dp = new long[10001];
+        // 0 부터 시작이긴하네
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= 90; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]);
+        }
+        System.out.println(dp[n]+" "+(n-2));
+    }
+}
