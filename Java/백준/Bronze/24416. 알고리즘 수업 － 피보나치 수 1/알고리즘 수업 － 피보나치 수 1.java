d25c8b245dba909b5d60df74ecdbb37652d3fd98
jngbnss <jngbnmn@naver.com>
2026-03-31 17:21:13 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 64 ms, Memory: 11492 KB -BaekjoonHub

--- Java/백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
+++ Java/백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.java
@@ -1,22 +1,37 @@
 import java.io.*;
 import java.util.*;
-import java.math.BigInteger;
 
 public class Main {
-    static int n, m;
-    static int arr[];
+    static int n;
     static long dp[];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        dp = new long[10001];
-        // 0 부터 시작이긴하네
+        //초
+        dp = new long[100];
+        for (int i = 0; i < 100; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+        //기
         dp[0] = 0;
         dp[1] = 1;
-        for (int i = 2; i <= 90; i++) {
-            dp[i] = (dp[i - 2] + dp[i - 1]);
-        }
+
+        //메
+        go(n);
         System.out.println(dp[n]+" "+(n-2));
+
+        //로
+
     }
+
+    static long go(int n) {
+        if (dp[n] != Integer.MAX_VALUE) {
+            return dp[n];
+        }
+
+        dp[n] = go(n - 1) + go(n - 2);
+        return dp[n];
+    }
+
 }
