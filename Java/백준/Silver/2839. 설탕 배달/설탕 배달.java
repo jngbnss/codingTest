4448b4646a8589fac284c5c15b17240978f793f2
jngbnss <jngbnmn@naver.com>
2026-03-31 17:42:54 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 11772 KB -BaekjoonHub

--- Java/백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ Java/백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -2,26 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m;
+    static int n;
     static long dp[];
+    static final int MX = 1000000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        dp = new long[50001];
-        Arrays.fill(dp, 9999);
+
+        dp = new long[5004];
+        Arrays.fill(dp, MX);
+
         dp[3] = 1;
         dp[5] = 1;
 
-        for (int i = 6; i <= n; i++) {
-            if (dp[i - 3] < 9999 || dp[i - 5] < 9999) {
-                dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
-            }
-        }
-        if (dp[n] >= 9999) {
+        go(n);
+        if (dp[n] >= MX) {
             System.out.println(-1);
         } else {
             System.out.println(dp[n]);
         }
+
     }
+
+    static long go(int n) {
+        if (n < 3) {
+            return MX;
+        }
+        if (n == 3 || n == 5) {
+            return 1;
+        }
+
+        if (dp[n] != MX) {
+            return dp[n];
+        }
+
+        return dp[n] = Math.min(go(n - 3), go(n - 5)) + 1;
+    }
+
+
 }
