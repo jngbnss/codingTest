56134e3921301953ddef2e8bbe32d3f9f0dbc5c5
jngbnss <jngbnmn@naver.com>
2026-04-01 10:49:45 +0900

    [Silver V] Title: Balanced String, Time: 76 ms, Memory: 17652 KB -BaekjoonHub

--- Java/백준/Silver/17520. Balanced String/Balanced String.java
+++ Java/백준/Silver/17520. Balanced String/Balanced String.java
@@ -1,21 +1,47 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static final int INF = 987654321;
+
+
+    static final int INF = 100004;
     static int n;
     static long memo[];
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        int cnt = n/2;
-        if(n%2!=0){
-            cnt++;
-        }
-        long ret = 1;
-        for(int i=0;i<cnt;i++){
-            ret = (ret*2)%16769023;
-        }
+
+        memo = new long[INF];
+        Arrays.fill(memo, -1);
+//        Arrays.fill(memo, INF);
+
+        long ret = dp(n);
         System.out.println(ret);
+    }
+
+    static long dp(int n) {
+        //기저사례
+        if (n == 0) {
+            memo[n] = 0;
+        }
+        if (n <= 2) {
+            memo[n] = 2;
+        }
+
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+
+        if (n / 2 == 0) {
+            int temp = n / 2;
+
+            return memo[n] = (dp(n - 2) * 2) % 16769023;
+        } else {
+            int temp = (n + 1 / 2);
+            return memo[n] = (dp(n - 2) * 2) % 16769023;
+        }
+
 
     }
 
