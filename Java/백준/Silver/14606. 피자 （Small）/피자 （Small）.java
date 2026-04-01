56e27aa0188a3150942d644aa4212552f7e7de1c
jngbnss <jngbnmn@naver.com>
2026-04-01 10:07:59 +0900

    [Silver V] Title: 피자 (Small), Time: 64 ms, Memory: 11516 KB -BaekjoonHub

--- Java/백준/Silver/14606. 피자 （Small）/피자 （Small）.java
+++ Java/백준/Silver/14606. 피자 （Small）/피자 （Small）.java
@@ -1,20 +1,36 @@
 import java.io.*;
 import java.util.*;
-public class Main {
-    static int n,m;
-    static long dp[];
 
-    public static void main(String[] args) throws IOException{
+public class Main {
+    static final int INF = 100;
+    static int n;
+    static long memo[];
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        dp = new long[100];
         n = Integer.parseInt(br.readLine());
-        dp[1] = 0;
-        dp[2] = 1;
-        for(int i=3;i<=10;i++){
-            int half = i/2;
-            int last = i-half;
-            dp[i] = dp[half]+dp[last]+(half*last);
-        }
-        System.out.println(dp[n]);
+
+        memo = new long[INF];
+        Arrays.fill(memo, -1);
+//        Arrays.fill(memo,INF);
+        System.out.println(dp(n));
+
     }
+
+    static long dp(int n) {
+        if (n == 1) {
+            return memo[n] = 0;
+        }
+        if (n == 2) {
+            return memo[n] =1;
+        }
+
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        int half = n / 2;
+        int last = n - half;
+        return memo[n] = dp(half) + dp(last) + (half * last);
+    }
+
 }
