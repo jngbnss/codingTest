d1e7d7b1bd95aaf1677a3658785a5c72dea67c9f
jngbnss <jngbnmn@naver.com>
2026-04-01 09:24:15 +0900

    [Silver V] Title: 거스름돈, Time: 140 ms, Memory: 17628 KB -BaekjoonHub

--- Java/백준/Silver/14916. 거스름돈/거스름돈.java
+++ Java/백준/Silver/14916. 거스름돈/거스름돈.java
@@ -1,26 +1,50 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
-public class Main {
-    static int n,m;
-    static long arr[];
 
-    public static void main(String[] args) throws IOException{
+public class Main {
+    static final int INF = 100004;
+    static int n;
+    static long memo[];
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-
-        arr = new long[100004];
-        Arrays.fill(arr,1000000);
-        arr[2] = 1;
-        arr[4] = 2;
-        arr[5] = 1;
-        for(int i=6;i<=n;i++){
-            arr[i] = Math.min(arr[i-2],arr[i-5]) +1;
-        }
-        if(arr[n]>=1000000){
+        memo = new long[n + 1];
+        Arrays.fill(memo, INF); //초기화
+        //아니면 여기서 2랑 5를 미리 입력해줘야하나?
+//        memo[2] = 1;
+//        memo[5] = 1;
+//        Arrays.fill(memo,INF);
+        long ret = dp(n);
+        if (memo[n] >= INF) {
             System.out.println(-1);
-        }else
-            System.out.println(arr[n]);
+        } else {
+            System.out.println(ret);
+//            System.out.println(memo[n]);
+        }
+    }
 
+    static long dp(int n) {
+        //if(n==0) return 0;
+        if (n <= 0) {
+            return INF;
+        }
+        //2와5의 동전
+        //기저사례
+        if (n == 2 || n == 5) {
+            return memo[n] = 1;
+        }
+
+        //메모이제이션
+        if (memo[n] != INF) {
+            return memo[n];
+        }
+
+        // 로직
+        long ret = Math.min(dp(n - 2), dp(n - 5)) + 1;
+        memo[n] = ret >= INF ? INF : ret;
+        //작은 거 구해야해서 -1말고 INF
+        return memo[n];
     }
 }
+
