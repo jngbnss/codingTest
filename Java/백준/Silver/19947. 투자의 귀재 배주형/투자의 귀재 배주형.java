2c62ad0429f87955b7ef9a8451c28acfdfb2a4d5
jngbnss <jngbnmn@naver.com>
2026-04-01 10:21:48 +0900

    [Silver V] Title: 투자의 귀재 배주형, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- Java/백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.java
+++ Java/백준/Silver/19947. 투자의 귀재 배주형/투자의 귀재 배주형.java
@@ -2,36 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m;
-    static long dp[];
+    static final int INF = 15;
+    static int n, h, y;
+    static long memo[];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        dp = new long[100];
+//        n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long h = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
 
-        //dp[1]은 하나밖에 없으니까?
-        dp[0] = h;
-        dp[1] = (long) (h * 1.05);
+        memo = new long[INF];
+        Arrays.fill(memo, -1);
+//        Arrays.fill(memo,INF);
 
-        for (int i = 2; i <= 12; i++) {
-            if (dp[i - 1] != 0) {
-                dp[i] =
-                        Math.max(dp[i], (long) (dp[i - 1] * 1.05)); // 기존에 있는 값이랑 비교해야하나?
+        memo[0] = h;
+        for (int i = 1; i <= y; i++) {
+            if (i - 1 >= 0) {
+                memo[i] = Math.max(memo[i], (long) (memo[i - 1] * 1.05));
             }
-
-            if (i >= 3) {
-                dp[i] =
-                        Math.max(dp[i], (long) (dp[i - 3] * 1.2));
+            if (i - 3 >= 0) {
+                memo[i] = Math.max(memo[i], (long) (memo[i - 3] * 1.2));
             }
-
-            if (i >= 5) {
-                dp[i] =
-                        Math.max(dp[i], (long) (dp[i - 5] * 1.35));
+            if (i - 5 >= 0) {
+                memo[i] = Math.max(memo[i], (long) (memo[i - 5] * 1.35));
             }
         }
-        System.out.println(dp[y]);
+        for (int i = 0; i < 10; i++) {
+//            System.out.println(memo[i]);
+        }
+        System.out.println(memo[y]);
+
     }
+
+
 }
