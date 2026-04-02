664826879db6ba35a6dc9d0900542601c4eb5609
jngbnss <jngbnmn@naver.com>
2026-04-02 11:12:35 +0900

    [Silver III] Title: 이친수, Time: 68 ms, Memory: 11520 KB -BaekjoonHub

---
+++ Java/백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int UNUSED = -1;
+    static int n;
+    static long memo[];
+    static int arr[];
+
+    public static void main(String[] args) throws IOException {
+        /**
+         * 설계다하고 진행하자
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        memo = new long[100];
+        arr = new int[100];
+        Arrays.fill(memo, UNUSED);
+
+        System.out.println(dp(n));
+
+    }
+
+    static long dp(int idx) {
+        //기저사례
+        if (idx == 0) {
+            return 0;
+        }
+        if (idx == 1) {
+            return 1;
+        }
+
+        if (memo[idx] != UNUSED) {
+            return memo[idx];
+        }
+
+        return memo[idx] = dp(idx - 1) + dp(idx - 2);
+    }
+
+}
