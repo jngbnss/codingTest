a69c411c01c30c582f58d4aea75f69eee6718dd2
jngbnss <jngbnmn@naver.com>
2026-04-03 10:13:32 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 41696 KB -BaekjoonHub

--- Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ Java/백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,42 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int UNUSED = 1_000_001;
     static int n;
-    static int memo[];
-
-    public static void main(String[] args) throws IOException {
-        /**
-         * 설계다하고 진행하자
-         */
+    static long memo[];
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        memo = new int[1000004];
-        Arrays.fill(memo, UNUSED);
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        n = Integer.parseInt(st.nextToken());
+
+        memo = new long[1000004];
+        Arrays.fill(memo,100000);
 
         System.out.println(dp(n));
-
     }
 
-    static int dp(int idx) {
-//        기저사례
-        if (idx == 1) {
+    static long dp(int idx){
+        if(idx==1){
             return memo[idx] = 0;
         }
+        if(idx<1){
+            return memo[idx] =99999999;
+        }
 
-        if (memo[idx] != UNUSED) {
+        if(memo[idx]!=100000){
             return memo[idx];
         }
 
-        if (idx % 3 == 0) {
-            memo[idx] = Math.min(memo[idx], dp(idx / 3) + 1);
+        if(idx%3==0){
+            memo[idx] = dp(idx/3)+1;
         }
-        if (idx % 2 == 0) {
-            memo[idx] = Math.min(memo[idx], dp(idx / 2) + 1);
+        if(idx%2==0){
+            memo[idx] = Math.min(memo[idx],dp(idx/2) +1);
         }
-        memo[idx] = Math.min(memo[idx], dp(idx - 1) + 1);
+            memo[idx] = Math.min(memo[idx],dp(idx-1)+1);
 
         return memo[idx];
     }
-
 }
