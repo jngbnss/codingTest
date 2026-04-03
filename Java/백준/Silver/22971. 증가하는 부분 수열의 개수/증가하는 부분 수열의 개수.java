c6096088d71e348cd8154e9955b942bd53a79572
jngbnss <jngbnmn@naver.com>
2026-04-03 12:04:14 +0900

    [Silver II] Title: 증가하는 부분 수열의 개수, Time: 216 ms, Memory: 28748 KB -BaekjoonHub

--- Java/백준/Silver/22971. 증가하는 부분 수열의 개수/증가하는 부분 수열의 개수.java
+++ Java/백준/Silver/22971. 증가하는 부분 수열의 개수/증가하는 부분 수열의 개수.java
@@ -2,34 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m;
-    static long arr[], dp[];
+    static int n;
+    static long memo[], arr[];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
+        memo = new long[1000004];
+        arr = new long[1000004];
+
+        Arrays.fill(memo, -1);
+        Arrays.fill(arr, 1);
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        arr = new long[n];
-        dp = new long[n];
-
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-
         }
-        Arrays.fill(dp, 1);
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j]) {
+            sb.append(dp(i)).append(" ");// i를 끝으로 하는 연속 합 출력
+        }
+        System.out.println(sb.toString());
+    }
 
-                    dp[i] = (dp[i] + dp[j]) % 998244353;
+    static long dp(int idx) {
+        if (idx == 0) {
+            return memo[idx] = 1;
+        }
 
-                }
+        if (memo[idx] != -1) {
+            return memo[idx];
+        }
+
+        long cnt = 1;
+
+        for(int j=0;j<idx;j++){
+            if(arr[j]<arr[idx]){
+                cnt = (cnt+dp(j)) % 998244353;
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            System.out.print(dp[i] + " ");
-        }
+        return memo[idx]=cnt;
     }
 }
