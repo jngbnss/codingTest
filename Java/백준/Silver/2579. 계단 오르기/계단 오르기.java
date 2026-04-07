3280ce5014375cb91388f7c1f2d4220921248db4
jngbnss <jngbnmn@naver.com>
2026-04-07 15:25:50 +0900

    [Silver III] Title: 계단 오르기, Time: 76 ms, Memory: 11636 KB -BaekjoonHub

--- Java/백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ Java/백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -2,46 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int UNUSED = -1;
     static int n;
-    static int[][] memo;
-    static int[] arr;
+    static int arr[];
+    static int memo[][];
 
+    /**
+     * memo[idx][cnt==0] 지금 밟고 있는 곳이 첫번째 이전에는 2칸을 뛰어 올라오은데 연속된 2칸 일수도있고 2칸을 뛰어온 2칸일수도있음
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n + 1];
-        memo = new int[n + 1][3];
-
+        memo = new int[n + 1][2];
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(memo[i], -1);
+        }
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(memo[i], UNUSED);
-        }
-
         if (n == 1) {
             System.out.println(arr[1]);
         } else {
-            System.out.println(Math.max(dp(n, 1), dp(n, 2)));
+            System.out.println(Math.max(dp(n, 0), dp(n, 1)));
         }
-
-
     }
 
     static int dp(int idx, int cnt) {
         if (idx == 1) {
-            return (cnt == 1) ? arr[1] : -1;
+            if (cnt == 0) {
+                //지금 밟고있는게 처음이니까
+                return arr[idx];
+            } else {
+                //첫번째를 2번 밟는건 말이 안되니까
+                return -10000;
+            }
         }
         if (idx == 2) {
-            if (cnt == 1) {
-                return arr[2]; // 0 -> 2 (2칸 점프)
-            }
-            if (cnt == 2) {
-                return arr[1] + arr[2]; // 1->2 (1칸이동)
+            if (cnt == 0) {
+                return arr[idx];
+            } else {
+                return arr[idx] + arr[idx - 1];
             }
         }
-
         if (idx <= 0) {
             return -1;
         }
@@ -49,14 +51,12 @@ public class Main {
             return memo[idx][cnt];
         }
 
-        int res = 0;
-        if (cnt == 1) { //내려가는 중에 첫번째 계단 앞으로 연속으로 내려가던 2개를 내려가던 상관없음
-            res = Math.max(dp(idx - 2, 1), dp(idx - 2, 2)) + arr[idx];
-            // 지금 밟고 있는게 첫번째가 되려면 무조건 2칸을 올라온 상태여야하는데
-            // 이전에도 2칸을 뛰어온 애일수도있고, 1,1을 밟아온애일수도있잖아
-        } else if (cnt == 2) {
-            res = dp(idx - 1, 1) + arr[idx];
+        int ret = 0;
+        if (cnt == 0) {
+            ret = Math.max(dp(idx - 2, 0), dp(idx - 2, 1)) + arr[idx];
+        } else {
+            ret = Math.max(ret, dp(idx - 1, 0) + arr[idx]);
         }
-        return memo[idx][cnt] = res;
+        return memo[idx][cnt] = ret;
     }
 }
