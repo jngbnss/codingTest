74eb0d1ea556b5e7176d267fec728f5f8e45dbdd
jngbnss <jngbnmn@naver.com>
2026-04-07 14:24:11 +0900

    [Silver V] Title: 거스름돈, Time: 136 ms, Memory: 25636 KB -BaekjoonHub

--- Java/백준/Silver/14916. 거스름돈/거스름돈.java
+++ Java/백준/Silver/14916. 거스름돈/거스름돈.java
@@ -2,49 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int INF = 100004;
     static int n;
     static long memo[];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        memo = new long[n + 1];
-        Arrays.fill(memo, INF); //초기화
-        //아니면 여기서 2랑 5를 미리 입력해줘야하나?
-//        memo[2] = 1;
-//        memo[5] = 1;
-//        Arrays.fill(memo,INF);
-        long ret = dp(n);
-        if (memo[n] >= INF) {
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        n = Integer.parseInt(st.nextToken());
+
+        memo = new long[1000004];
+        Arrays.fill(memo, 100000);
+
+        if (dp(n) >= 100000) {
             System.out.println(-1);
         } else {
-            System.out.println(ret);
-//            System.out.println(memo[n]);
+            System.out.println(memo[n]);
         }
     }
 
-    static long dp(int n) {
-        //if(n==0) return 0;
-        if (n <= 0) {
-            return INF;
-        }
-        //2와5의 동전
-        //기저사례
-        if (n == 2 || n == 5) {
-            return memo[n] = 1;
+    static long dp(int idx) {
+        if (idx == 0) {
+            return memo[idx] = 0;
         }
 
-        //메모이제이션
-        if (memo[n] != INF) {
-            return memo[n];
+        if (memo[idx] != 100000) {
+            return memo[idx];
         }
 
-        // 로직
-        long ret = Math.min(dp(n - 2), dp(n - 5)) + 1;
-        memo[n] = ret >= INF ? INF : ret;
-        //작은 거 구해야해서 -1말고 INF
-        return memo[n];
+        if (idx - 2 >= 0) {
+            memo[idx] = dp(idx - 2) + 1;
+        }
+        if (idx - 5 >= 0) {
+            memo[idx] = Math.min(memo[idx], dp(idx - 5) + 1);
+        }
+
+        return memo[idx];
     }
 }
-
