6b622f7832ef983260a182c184be559c54d4b300
jngbnss <jngbnmn@naver.com>
2026-04-08 16:00:29 +0900

    [Silver II] Title: 증가하는 부분 수열의 개수, Time: 292 ms, Memory: 14472 KB -BaekjoonHub

--- Java/백준/Silver/22971. 증가하는 부분 수열의 개수/증가하는 부분 수열의 개수.java
+++ Java/백준/Silver/22971. 증가하는 부분 수열의 개수/증가하는 부분 수열의 개수.java
@@ -2,47 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n;
-    static long memo[], arr[];
+    static int n,arr[];
+    static long memo[];
+    static long ret;
+
+    static long dp(int idx) {
+        if (memo[idx] >= 0) {
+            return memo[idx];
+        }
+        long sum = 1;
+        for(int j=0;j<idx;j++){
+            if(arr[j]<arr[idx]){
+                sum = (sum+dp(j))%998244353;
+            }
+        }
+        return memo[idx] = sum;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        memo = new long[1000004];
-        arr = new long[1000004];
-
-        Arrays.fill(memo, -1);
-        Arrays.fill(arr, 1);
-
+        arr =new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        StringBuilder sb = new StringBuilder();
+        memo = new long[5004];
+        Arrays.fill(memo, -1);
         for (int i = 0; i < n; i++) {
-            sb.append(dp(i)).append(" ");// i를 끝으로 하는 연속 합 출력
+            System.out.print(dp(i)+" ");
         }
-        System.out.println(sb.toString());
     }
 
-    static long dp(int idx) {
-        if (idx == 0) {
-            return memo[idx] = 1;
-        }
-
-        if (memo[idx] != -1) {
-            return memo[idx];
-        }
-
-        long cnt = 1;
-
-        for(int j=0;j<idx;j++){
-            if(arr[j]<arr[idx]){
-                cnt = (cnt+dp(j)) % 998244353;
-            }
-        }
-
-        return memo[idx]=cnt;
-    }
 }
