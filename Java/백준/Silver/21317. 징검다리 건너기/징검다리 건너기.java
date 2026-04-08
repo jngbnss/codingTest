26458695d76878911f30ee023d38487220eaaa62
jngbnss <jngbnmn@naver.com>
2026-04-08 17:48:00 +0900

    [Silver I] Title: 징검다리 건너기, Time: 64 ms, Memory: 11604 KB -BaekjoonHub

--- Java/백준/Silver/21317. 징검다리 건너기/징검다리 건너기.java
+++ Java/백준/Silver/21317. 징검다리 건너기/징검다리 건너기.java
@@ -2,65 +2,44 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static final int INF = 100000000;
+    static int n, k;
+    static int arr[][];
+    static int memo[][];
+
+    static int dp(int idx, int flag) {
+        if (idx == n) {
+            return 0;
+        }
+        if (idx > n) {
+            return 987654312;
+        }
+        if (memo[idx][flag] != -1) {
+            return memo[idx][flag];
+        }
+        int temp = dp(idx + 1, flag) + arr[idx][1];
+        if (idx + 2 <= n) {
+            temp = Math.min(temp, dp(idx + 2, flag) + arr[idx][2]);
+        }
+        if (idx + 3 <= n && flag == 0) {
+            temp = Math.min(temp, dp(idx + 3, 1) + k);
+        }
+        return memo[idx][flag] = temp;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        if (n == 1) {
-            System.out.println(0);
-            return;
+        n = Integer.parseInt(br.readLine());
+        arr = new int[n + 1][3];
+        memo = new int[n + 1][2];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(memo[i], -1);
         }
-
-        int[][] cost = new int[n][2];
         for (int i = 1; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            cost[i][0] = Integer.parseInt(st.nextToken()); //작은 점프
-            cost[i][1] = Integer.parseInt(st.nextToken()); // 큰 점프
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
         }
-
-        int k = Integer.parseInt(br.readLine());
-
-        // dp[돌 번호][k 점프 사용 여부 0,1]
-        int[][] dp = new int[n + 1][2];
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], INF);
-        }
-
-        //시작점 초기화
-        dp[1][0] = 0;
-
-        for (int i = 1; i < n; i++) {
-            // 아직 k 점프를 안 쓴 경우에서 파생
-            if (dp[i][0] != INF) {
-                //작은 점프 1칸
-                if (i + 1 <= n) {
-                    dp[i + 1][0] = Math.min(dp[i + 1][0], dp[i][0] + cost[i][0]);
-
-                }
-                if (i + 2 <= n) {
-                    dp[i + 2][0] = Math.min(dp[i + 2][0], dp[i][0] + cost[i][1]);
-                }
-                //매우 큰 점프(3칸) -> 사용 후 상태 1로 전이
-                if (i + 3 <= n) {
-                    dp[i + 3][1] = Math.min(dp[i + 3][1], dp[i][0] + k);
-                }
-            }
-
-            // 2. 이미 k 점프를 쓴 경우에서 파생
-            if (dp[i][1] != INF) { // 엥?
-                //작은 점프
-                if (i + 1 <= n) {
-                    dp[i + 1][1] = Math.min(dp[i + 1][1], dp[i][1] + cost[i][0]);
-
-                }
-                if (i + 2 <= n) {
-                    dp[i + 2][1] = Math.min(dp[i + 2][1], dp[i][1] + cost[i][1]);
-                }
-            }
-        }
-
-        System.out.println(Math.min(dp[n][0], dp[n][1]));
-
+        k = Integer.parseInt(br.readLine());
+        System.out.println(dp(1, 0));
     }
 }
