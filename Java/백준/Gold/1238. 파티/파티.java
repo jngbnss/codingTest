4cd4943953ac4cd2ef7c72c26538d777e8460e91
jngbnss <jngbnmn@naver.com>
2026-04-16 07:25:14 +0900

    [Gold III] Title: 파티, Time: 428 ms, Memory: 52096 KB -BaekjoonHub

--- Java/백준/Gold/1238. 파티/파티.java
+++ Java/백준/Gold/1238. 파티/파티.java
@@ -1,90 +1,99 @@
-import java.io.*;
-import java.util.*;
-public class Main {
-	static final int INF = Integer.MAX_VALUE;
-	static int n,m,x;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		 n = Integer.parseInt(st.nextToken());
-		 m = Integer.parseInt(st.nextToken());
-		 x = Integer.parseInt(st.nextToken());
-		
-		//n개의 집이있고
-		list = new ArrayList[n+1];
-		reverseList = new ArrayList[n+1];
-		
-		for(int i=0;i<=n;i++) {
-			list[i] = new ArrayList<Edge>();
-			reverseList[i] = new ArrayList<Edge>();
-		}
-		
-		
-		// a에서 b로 가는 비용이 나옴
-		for(int i=0;i<m;i++) {
-			st = new StringTokenizer(br.readLine());
-			int a= Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			list[a].add(new Edge(b,cost));
-			reverseList[b].add(new Edge(a,cost));
-		}
-		
-		//3.다익스트라 실행(x에서 시작)
-		int[] distToX = dijk(reverseList);
-		int[] distFromX = dijk(list);
-		
-		int maxTime = 0;
-		for(int i=1;i<=n;i++) {
-			if(distToX[i]!=INF && distFromX[i]!=INF) {
-				maxTime = Math.max(maxTime, distToX[i]+distFromX[i]);
-			}
-		}
-		System.out.println(maxTime);
-		
-		
-	}
-	
-	//다익스트라 알고리즘 (인자값으로 리스트를 받아 공용으로 사용)
-	static int[] dijk(ArrayList<Edge>[]adj) {
-		PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
-		int[]dist = new int[n+1];
-		Arrays.fill(dist,INF);
-		
-		dist[x] = 0;
-		pq.add(new Edge(x,0));
-		while(!pq.isEmpty()) {
-			Edge current = pq.poll();
-			
-			if(current.cost>dist[current.to])continue;
-			
-			for(Edge next:adj[current.to]) {
-				if(dist[next.to]>dist[current.to]+next.cost) {
-					dist[next.to]= dist[current.to]+next.cost;
-					pq.add(new Edge(next.to,dist[next.to]));
-				}
-			}
-		}
-		return dist;
-	}
-	static ArrayList<Edge>[]list;
-	static ArrayList<Edge>[]reverseList;
-	
-	static class Edge implements Comparable<Edge>{
-		int to;
-		int cost;
-		public Edge(int to, int cost) {
-			super();
-			this.to = to;
-			this.cost = cost;
-		}
-		@Override
-		public int compareTo(Edge o) {
-			// TODO Auto-generated method stub
-			return Integer.compare(this.cost, o.cost);
-		}
-		
-		
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
+public class Main {
+    static final int INF = Integer.MAX_VALUE;
+    static int n, m, x, a, b, c;
+    static int dist[];
+    static ArrayList<Node> list[];
+    static int arr[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+        dist = new int[n + 1];
+        arr = new int[n + 1];
+        Arrays.fill(dist, INF);
+        list = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            list[a].add(new Node(b, c));
+        }
+        int ret = 0;
+
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist, INF);
+            if (i == x) {
+                continue;
+            }
+            go(i);
+            arr[i] = dist[x];
+        }
+        Arrays.fill(dist, INF);
+        go(x);
+        for (int i = 1; i <= n; i++) {
+            if (i == x) {
+                continue;
+            }
+            arr[i] += dist[i];
+        }
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            ret = Math.max(ret, arr[i]);
+            if (arr[i] == ret) {
+                result = i;
+            }
+        }
+        System.out.println(ret);
+    }
+
+    static void go(int i) {
+        dist[i] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(i, 0));
+        while (!pq.isEmpty()) {
+            Node poll = pq.poll();
+            int now = poll.to;
+            int cost = poll.cost;
+            if (cost != dist[now]) {
+                continue;
+            }
+            for (Node node : list[now]) {
+                int nextCost = node.cost + cost;
+                if (nextCost < dist[node.to]) {
+                    dist[node.to] = nextCost;
+                    pq.offer(new Node(node.to, nextCost));
+                }
+            }
+        }
+    }
+
+    static class Node implements Comparable<Node> {
+        int to;
+        int cost;
+
+        public Node(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
 }
