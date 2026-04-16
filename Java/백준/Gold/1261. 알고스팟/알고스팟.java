265c27792edcaf0c59418f64a08398502e5a4713
jngbnss <jngbnmn@naver.com>
2026-04-17 07:43:36 +0900

    [Gold IV] Title: 알고스팟, Time: 212 ms, Memory: 18788 KB -BaekjoonHub

--- Java/백준/Gold/1261. 알고스팟/알고스팟.java
+++ Java/백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,94 +1,63 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static final int INF = 204;
-    static int n, m;
-    static int[][] dist;
-    static int[][] arr;
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, -1, 0, 1};
+    static int m, n;
+    static int arr[][], dist[][];
+    static int dy[] = {-1, 0, 1, 0};
+    static int dx[] = {0, -1, 0, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
-
         arr = new int[n][m];
-        dist = new int[n][m];
         for (int i = 0; i < n; i++) {
-            Arrays.fill(dist[i], INF);
-        }
-        for (int i = 0; i < n; i++) {
+//            st = new StringTokenizer(br.readLine());
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                arr[i][j] = line.charAt(j) - '0';
-
+                arr[i][j] = (line.charAt(j) - '0');
             }
         }
 
-        dijk(0, 0, arr[0][0]);
+        dist = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+
+        }
+        dijk(0, 0, 0);
         System.out.println(dist[n - 1][m - 1]);
-
-
     }
 
     static void dijk(int y, int x, int cost) {
-        dist[y][x] = 0;
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(y, x, dist[y][x] ));
+        dist[y][x] = cost;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(((a, b) -> a[2] - b[2]));
+        pq.offer(new int[]{y, x, cost});
 
         while (!pq.isEmpty()) {
-            Node poll = pq.poll();
-            y = poll.y;
-            x = poll.x;
-            cost = poll.cost;
-
+            int[] poll = pq.poll();
+            y = poll[0];
+            x = poll[1];
+            cost = poll[2];
             if (cost != dist[y][x]) {
                 continue;
             }
-            if (y == n-1 && x == m-1) {
+            if (y == n - 1 && x == m - 1) {
                 return;
             }
             for (int i = 0; i < 4; i++) {
                 int ny = y + dy[i];
                 int nx = x + dx[i];
-
-                if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
                     continue;
                 }
-
                 int nextCost = dist[y][x] + arr[ny][nx];
                 if (nextCost < dist[ny][nx]) {
                     dist[ny][nx] = nextCost;
-                    pq.offer(new Node(ny, nx, nextCost));
+                    pq.offer(new int[]{ny, nx, nextCost});
                 }
-
             }
-
-        }
-
-    }
-
-    static class Node implements Comparable<Node> {
-        int y;
-        int x;
-        int cost;
-
-        public Node(int y, int x, int cost) {
-            this.y = y;
-            this.x = x;
-            this.cost = cost;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.cost - o.cost;
         }
     }
 }
