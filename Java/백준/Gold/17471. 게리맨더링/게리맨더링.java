e1548f8e3ec2a8f2b0e8d3a455984fe454769825
jngbnss <jngbnmn@naver.com>
2026-04-20 21:50:21 +0900

    [Gold III] Title: 게리맨더링, Time: 84 ms, Memory: 14180 KB -BaekjoonHub

--- Java/백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ Java/백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,116 +1,109 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-/**
- * 입력받고
- * 연결하고, 안하고 해서
- * 다 나오면 거기서 진행해보자
- * 몰빵인가?
- * 연결이 끊겼는가?
- * 비교했을때 최소차이가 정답이고
- * 만약 두 선거구로 나눌 수 없으면 -1
- * 8:09
- */
+//9:30
 public class Main {
     static int n;
-    static int populations[];
+    static int pop[];
+    static ArrayList<Integer> list[];
+    static int visited[];
+    static int ret = Integer.MAX_VALUE;
 
-    static ArrayList<Integer>list[];
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-
-        populations = new int[n+1];
+        pop = new int[n + 4];
+        visited = new int[n + 4];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1;i<=n;i++){
-            int x = Integer.parseInt(st.nextToken());
-            populations[i] = x;
+        for (int i = 1; i <= n; i++) {
+            pop[i] = Integer.parseInt(st.nextToken());
         }
-
-        list = new ArrayList[n+1];
-
-        for(int i=1;i<=n;i++){
+        list = new ArrayList[n + 4];
+        for (int i = 1; i <= n; i++) {
             list[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            int countOfSector = Integer.parseInt(st.nextToken());
-            for(int j=0;j<countOfSector;j++){
+            int count = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < count; j++) {
                 list[i].add(Integer.parseInt(st.nextToken()));
-                // 이건 연결 되어있는지 확인할때 쓰는거니까
-            }
-
-        }
-        count = new boolean[n+1];
-        // 카운트 불리언 배열로 이번 구역나눌때 포함하냐 마냐로 구성할것
-        go(1);
-        System.out.println(ret==Integer.MAX_VALUE?-1:ret);
-
-    }
-    static boolean count[];
-    static int ret = Integer.MAX_VALUE;
-    static boolean isCon(ArrayList<Integer> team){
-        int cnt = 1;
-        int localVisited[] = new int[n+1];
-        Integer i = team.get(0);
-        localVisited[i] = 1;
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-        q.offer(i);
-        while(!q.isEmpty()){
-            Integer now = q.poll();
-            for (Integer next : list[now]) {
-                if(localVisited[next]==0&&team.contains(next)){
-                    cnt++;
-                    localVisited[next] = 1;
-                    q.offer(next);
-                }
             }
         }
-        return cnt==team.size();
+
+        //리스트 연결 확인했으니까
+        go(1); // 방문처리 하고 말고로 진행
+        System.out.println(ret == Integer.MAX_VALUE ? -1 : ret);
+
     }
-    static void go(int idx){
-        if(idx==n+1){
+
+    static void go(int idx) {
+        if (idx > n) {
+            //
             ArrayList<Integer> teamA = new ArrayList<>();
             ArrayList<Integer> teamB = new ArrayList<>();
-            for(int i=1;i<=n;i++){
-                if(count[i] ==true){
+
+            for (int i = 1; i <= n; i++) {
+                if (visited[i] == 1) {
                     teamA.add(i);
-                }else{
+                } else {
                     teamB.add(i);
                 }
             }
-            // 카운트로 a랑 b구분했으니까
-
-            // 몰빵인가?
-            if(teamA.isEmpty()||teamB.isEmpty()){
+            if (teamA.isEmpty() || teamB.isEmpty()) {
                 return;
             }
-
-            // 연결이 잘 되어있는가?
-
-            if(isCon(teamA)&&isCon(teamB)){
-                // -1 인가 아니면 차이를 구하자
-                int popA = 0;
-                int popB =0;
-                for (int i = 0; i < teamA.size(); i++) {
-                    Integer i1 = teamA.get(i);
-                    popA+=populations[i1];
+            if (isCon(teamA, 1) && isCon(teamB, 0)) {
+                //둘다 연결되어있으면
+                int a = 0;
+                int b = 0;
+                for (int i = 1; i <= n; i++) {
+                    if (visited[i] == 1) {
+                        a += pop[i];
+                    } else {
+                        b += pop[i];
+                    }
                 }
-                for (int i = 0; i < teamB.size(); i++) {
-                    Integer i1 = teamB.get(i);
-                    popB+=populations[i1];
-                }
-                int temp = Math.abs(popA-popB);
-                ret = Math.min(temp,ret);
-
+                ret = Math.min(ret, Math.abs(a - b));
             }
             return;
         }
-        count[idx]= false;
-        go(idx+1);
-        count[idx]= true;
-        go(idx+1);
 
-
+        visited[idx] = 1;
+        go(idx + 1);
+        visited[idx] = 0;
+        go(idx + 1);
 
     }
+
+    static boolean isCon(ArrayList<Integer> subList, int type) {
+
+        int[] localVisited = new int[n + 4];
+        int cnt = 1;
+        localVisited[subList.get(0)] = 1;
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.offer(subList.get(0));
+        while (!q.isEmpty()) {
+            Integer poll = q.poll();
+            for (Integer i : list[poll]) {
+//                if (localVisited[i] == 0 && subList.contains(i)) {
+                if (localVisited[i] == 0 && visited[i] == type) {
+                    cnt++;
+                    localVisited[i] = 1;
+                    q.offer(i);
+                }
+
+            }
+        }
+        if (subList.size() == cnt) {
+            return true;
+        }
+
+        return false;
+    }
 }
+//0948
