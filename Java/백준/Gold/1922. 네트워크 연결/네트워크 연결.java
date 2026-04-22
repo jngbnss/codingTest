a0984bd36d41d5d2d9862f7bdca513bcfbd2f1e5
jngbnss <jngbnmn@naver.com>
2026-04-22 09:44:53 +0900

    [Gold IV] Title: 네트워크 연결, Time: 420 ms, Memory: 47644 KB -BaekjoonHub

--- Java/백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ Java/백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,78 +1,84 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-    static int n,m,a,b,c;
+    static int n, m;
+    static int a, b, c;
     static int root[];
+    static int ret;
+    static ArrayList<Node> list;
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-
-        root = new int[n+1];
-        for(int i=0;i<n+1;i++){
-            root[i] = i;
-        }
-
-        list = new ArrayList<>();
-
-        for(int i=0;i<m;i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            c = Integer.parseInt(st.nextToken());
-
-            list.add(new Edge(a,b,c));
-
-        }
-
-        Collections.sort(list);
-
-        int total = 0;
-        int cnt = 0;
-        for(Edge e:list){
-            if(find(e.a)!=find(e.b)){
-                union(e.a,e.b);
-                total+=e.c;
-                cnt++;
-
-                if(cnt==n-1){
-                    break;
-                }
-            }
-        }
-        System.out.println(total);
-
-
-    }
-    static void union(int a,int b){
-        a = root[a];
-        b = root[b];
-
-        if(a!=b){
-            root[a] =b;
-        }
-    }
-    static int find(int x){
-        if(x==root[x]){
+    static int find(int x) {
+        if (x == root[x]) {
             return x;
         }
         return root[x] = find(root[x]);
     }
-    static ArrayList<Edge>list;
 
-    static class Edge implements Comparable<Edge>{
-        int a,b,c;
+    static void union(int a, int b) {
+        a = root[a];
+        b = root[b];
+        if (a != b) {
+            root[a] = b;
+        }
+    }
 
-        public Edge(int a, int b, int c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        root = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            root[i] = i;
+        }
+        list = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            list.add(new Node(a, b, c));
+
+        }
+        Collections.sort(list);
+
+        int cnt = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            Node node = list.get(i);
+            int from = node.from;
+            int to = node.to;
+            int cost = node.cost;
+            if (find(from) != find(to)) {
+                union(from, to);
+                cnt++;
+                ret += cost;
+
+            }
+            if (cnt == n - 1) {
+                break;
+            }
+        }
+        System.out.println(ret);
+
+
+    }
+
+    static class Node implements Comparable<Node> {
+        int from;
+        int to;
+        int cost;
+
+        public Node(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
         }
 
         @Override
-        public int compareTo(Edge o) {
-            return this.c - o.c;
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
         }
     }
 }
