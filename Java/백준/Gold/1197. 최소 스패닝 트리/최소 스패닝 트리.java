5f7a7cb1c1e8645545c52d15e4e9996c6a8793c6
jngbnss <jngbnmn@naver.com>
2026-04-22 09:30:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 416 ms, Memory: 49184 KB -BaekjoonHub

--- Java/백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ Java/백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,77 +1,87 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
 public class Main {
-    static int n,m,ret;
-    static class Node implements Comparable<Node>{
-        int from;
-        int to;
-        int w;
+    static int v, e, a, b, c, ret;
+    static int arr[], root[];
+    static ArrayList<Node> list;
 
-        public Node(int from, int to, int w) {
-            this.from = from;
-            this.to = to;
-            this.w = w;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.w-o.w;
-        }
-    }
-    static ArrayList<Node>list;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        ret =0 ;
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+
+        arr = new int[v + 1];
         list = new ArrayList<>();
-        for(int i=0;i<m;i++){
+        for (int i = 0; i < e; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            list.add(new Node(a,b,c));
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            list.add(new Node(a, b, c));
 
         }
         Collections.sort(list);
 
-        root = new int[n+1];
-        for(int i=0;i<=n;i++){
+        root = new int[v + 1];
+        for (int i = 0; i <= v; i++) {
             root[i] = i;
         }
-
         int cnt = 0;
-        for(int i=0;i<list.size();i++){
-            
+        for (int i = 0; i < list.size(); i++) {
             Node node = list.get(i);
             int from = node.from;
             int to = node.to;
-            int w = node.w;
-            if(find(from)!=find(to)){
+            int cost = node.cost;
+            if (find(from) != find(to)) {
                 cnt++;
-                uion(from,to);
-                ret+=w;
+                union(from, to);
+                ret += cost;
             }
-            if(cnt==n-1){
+            if (cnt == v - 1) {
                 break;
             }
         }
         System.out.println(ret);
+
+        // 아무데서나 해도 상관없음
     }
-    static int root[];
-    static void uion(int a,int b){
-        a = find(a);
-        b = find(b);
-        if(a!=b){
-            root[a] = b;
-        }
-    }
-    static int find(int x){
-        if(x==root[x]){
+
+    static int find(int x) {
+        if (x == root[x]) {
             return x;
         }
         return root[x] = find(root[x]);
+    }
 
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            root[a] = b;
+
+        }
+    }
+
+    static class Node implements Comparable<Node> {
+        int from;
+        int to;
+        int cost;
+
+        public Node(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
     }
 }
