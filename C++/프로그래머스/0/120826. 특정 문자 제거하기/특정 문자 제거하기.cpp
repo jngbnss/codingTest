925d68f561a1f942a4c78cac151a522f9a75c5b2
jngbnss <jngbnmn@naver.com>
2026-05-14 17:29:10 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 3.94 MB -BaekjoonHub

--- C++/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
+++ C++/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -3,13 +3,11 @@
 
 using namespace std;
 
-string solution(string my_string,string letter) {
+string solution(string my_string, string letter) {
     string answer = "";
     for(auto i:my_string){
         if(i==letter[0]) continue;
-        else{
-            answer+=i;
-        }
+        else answer+=i;
     }
     return answer;
 }
