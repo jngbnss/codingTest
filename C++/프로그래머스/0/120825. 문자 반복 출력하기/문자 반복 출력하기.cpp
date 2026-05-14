3e1ad8b396daaf732594cab098761884af312a47
jngbnss <jngbnmn@naver.com>
2026-05-14 17:30:13 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- C++/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
+++ C++/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 string solution(string my_string, int n) {
     string answer = "";
-    for(auto i: my_string){
+    for(auto i:my_string){
         for(int j=0;j<n;j++){
             answer+=i;
         }
