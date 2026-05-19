8d11cbf7a9e47e69269b615bc4ba76213097fab1
jngbnss <jngbnmn@naver.com>
2026-05-19 13:38:14 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120849. 모음 제거/모음 제거.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    for(auto i:my_string){
+        if(i=='a'
+          ||i=='e'
+          ||i=='i'
+           ||i=='o'
+           ||i=='u'
+          
+          ) continue;
+        else answer+=i;
+    }
+    return answer;
+}
