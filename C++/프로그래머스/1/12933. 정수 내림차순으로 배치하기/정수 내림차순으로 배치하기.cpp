1afb024099c5c9b64eafad0b96f76cabaab9f55e
jngbnss <jngbnmn@naver.com>
2026-05-20 09:32:11 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 3.93 MB -BaekjoonHub

--- C++/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ C++/프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -1,23 +1,16 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+
 using namespace std;
 
 long long solution(long long n) {
-    long long answer = 0;
-    vector<int>v;
-    while(n>0){
-        v.push_back(n%10);
-        n/=10;
-        
-        
-        
-    }
-    sort(v.rbegin(),v.rend());
-    for(auto i:v){
+    long long answer =0;
+    string s = to_string(n);
+    sort(s.rbegin(), s.rend());
+  for(auto i:s){
         answer*=10;
-        answer+=i;
-    }
+      answer+=i-'0';
+  }
     return answer;
 }
