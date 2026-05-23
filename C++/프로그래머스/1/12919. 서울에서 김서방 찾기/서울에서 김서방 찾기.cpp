82ea78b12e485b81f8a7e5b0e6819e9cfd8c30ce
jngbnss <jngbnmn@naver.com>
2026-05-23 18:23:23 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 3.94 MB -BaekjoonHub

--- C++/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
+++ C++/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -5,12 +5,5 @@ using namespace std;
 
 string solution(vector<string> seoul) {
     string answer = "";
-    int idx = 0;
-    for(int i=0;i<seoul.size();i++){
-        if(seoul[i]=="Kim"){
-            idx = i;
-        }
-    }
-    answer = "김서방은 "+to_string(idx)+"에 있다";
     return answer;
 }
